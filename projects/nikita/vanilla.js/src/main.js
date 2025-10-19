7c4347b59063c8fec036287e78323c74e81b0deb
Nikita (front) <nikita_front@mvpproject.io>
2025-10-19 15:21:07 +0300

    feature(nikita js): realease base version

--- projects/nikita/vanilla.js/src/main.js
+++ projects/nikita/vanilla.js/src/main.js
@@ -13,7 +13,14 @@ const config = {
 //event handlers
 const onDraggableStart = (event) => {
   const targetId = event.target.dataset.draggId
+
   event.dataTransfer.setData("text/plain", targetId)
+  event.target.classList.add('dragging')
+  event.dataTransfer.effectAllowed = 'move'
+}
+
+const onDraggableEnd = (event) => {
+  event.target.classList.remove('dragging')
 }
 
 const onDrop = (event) => {
@@ -22,11 +29,17 @@ const onDrop = (event) => {
 
 const onDragover = (event) => {
   event.preventDefault()
-  event.dataTransfer.dropEffect = "move"
+  const dragging = document.querySelector('.dragging')
+  if (!dragging) return
+
+  const container = document.querySelector(`[data-dragg-id="${dragging.dataset.parentId}"]`)
+  console.log(event.target)
+  container.insertBefore(dragging, event.target)
 }
 
 const addDraggbleEvents = (node) => {
   node.addEventListener('dragstart', onDraggableStart)
+  node.addEventListener('dragend', onDraggableEnd)
 }
 
 const addEventForDraggbleZone = (node) => {
@@ -35,19 +48,23 @@ const addEventForDraggbleZone = (node) => {
 }
 
 const main = () => {
+
 const draggContainers = document.querySelectorAll(`[${config.dragg_container}]`)
 if(!draggContainers) return
 
 draggContainers.forEach((draggContainer) => {
   addEventForDraggbleZone(draggContainer)
+  createRandomId(draggContainer)
+  
   const children = nodeListToArray(getChildren(draggContainer))
 
   children.forEach(node => {
     createRandomId(node)
+    node.dataset.parentId = draggContainer.dataset.draggId
     setDraggbleAttribute(node)
     addDraggbleEvents(node)
   })
-})
-}
+
+})}
 
 document.addEventListener('DOMContentLoaded', main)
